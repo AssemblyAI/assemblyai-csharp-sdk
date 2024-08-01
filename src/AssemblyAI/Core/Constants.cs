45e74bdd5f90cf4d1cf91587cf2d7ad61c58b97e
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-08-01 14:33:37 +0200

    Merge Constants.cs, keep internal

--- src/AssemblyAI/Core/Constants.cs
+++ src/AssemblyAI/Core/Constants.cs
@@ -1,6 +1,7 @@
 namespace AssemblyAI.Core;
 
-public static class Constants
+internal static class Constants
 {
+    internal const string Version = "1.0.0-beta";
     public const string DateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffK";
 }
