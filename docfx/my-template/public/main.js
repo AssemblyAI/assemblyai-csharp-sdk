3450f63be6c1f3c49fab3022e00e4b3d61bafbe1
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-08-02 13:44:12 +0200

    Add docfx

---
+++ docfx/my-template/public/main.js
@@ -0,0 +1,20 @@
+export default {
+  defaultTheme: 'dark',
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/AssemblyAI/assemblyai-csharp-sdk',
+      title: 'AssemblyAI C# .NET SDK GitHub'
+    },
+    {
+      icon: 'discord',
+      href: 'https://assemblyai.com/discord',
+      title: 'AssemblyAI Discord'
+    },
+    {
+      icon: 'twitter',
+      href: 'https://x.com/AssemblyAI',
+      title: 'AssemblyAI Twitter'
+    }
+  ]
+}
