e6d545667309199d2b7c97b3c54567886f7dd067
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-08-11 15:31:44 -0400

    Workaround for getting error message in exception message (doesn't work atm)

--- src/AssemblyAI/Core/Public/ApiException.cs
+++ src/AssemblyAI/Core/Public/ApiException.cs
@@ -4,7 +4,7 @@ namespace AssemblyAI;
 /// This exception type will be thrown for any non-2XX API responses.
 /// </summary>
 public class ApiException(string message, int statusCode, object body)
-    : AssemblyAIException(message)
+    : AssemblyAIException(body is Error error ? error.Error_ : message)
 {
     /// <summary>
     /// The error code of the response that triggered the exception.
@@ -14,5 +14,5 @@ public class ApiException(string message, int statusCode, object body)
     /// <summary>
     /// The body of the response that triggered the exception.
     /// </summary>
-    public object Body { get; } = body;
+    internal object Body { get; } = body;
 }
