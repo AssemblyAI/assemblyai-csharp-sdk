ad91d11f012e30e690d71f1f21713017eb6270b1
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-08-11 22:30:13 -0400

    Retrieve message from error JSON boyd

--- src/AssemblyAI/Core/Public/ApiException.cs
+++ src/AssemblyAI/Core/Public/ApiException.cs
@@ -1,10 +1,13 @@
+using System.Text.Json;
+using AssemblyAI.Core;
+
 namespace AssemblyAI;
 
 /// <summary>
 /// This exception type will be thrown for any non-2XX API responses.
 /// </summary>
 public class ApiException(string message, int statusCode, object body)
-    : AssemblyAIException(body is Error error ? error.Error_ : message)
+    : AssemblyAIException(GetMessageFromJsonBody(message, body))
 {
     /// <summary>
     /// The error code of the response that triggered the exception.
@@ -15,4 +18,18 @@ public class ApiException(string message, int statusCode, object body)
     /// The body of the response that triggered the exception.
     /// </summary>
     internal object Body { get; } = body;
+    
+    private static string GetMessageFromJsonBody(string message, object body)
+    {
+        if (body is not string stringBody) return message;
+        try
+        {
+            var errorObject = JsonUtils.Deserialize<Error>(stringBody);
+            return errorObject.Error_;
+        }
+        catch (JsonException)
+        {
+            return stringBody;
+        }
+    }
 }
